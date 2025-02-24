f29491c47ee809b2c0a4a2130692a7b40cc7d19e
minjae <minjagot@gmail.com>
2025-02-24 21:42:49 +0900

    back init

---
+++ back/src/main/java/com/marry_invite/MarryInviteApplication.java
@@ -0,0 +1,13 @@
+package com.marry_invite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarryInviteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarryInviteApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/marry_invite/MarryInviteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marry_invite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarryInviteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
