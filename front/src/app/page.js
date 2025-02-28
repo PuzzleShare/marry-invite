4dc078a35531e9d49c100ee7cd72d4299b5fd674
jeonginwoo <jiw413@naver.com>
2025-02-28 19:48:50 +0900

    feat: login dialog창

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -2,17 +2,16 @@
 
 import * as React from 'react';
 import Image from "next/image";
-import styles from "../styles/page.module.css";
+import styles from "@/styles/page.module.css";
 
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 
-import Header from "../components/Header"
-import Main from "../components/Main"
-import Mypage from "../components/Mypage"
-import CreatePage from "../components/CreatePage"
-import CreateButton from "../components/CreateButton"
-import Login from "../components/Login"
+import Header from "@/components/Header"
+import Main from "@/components/Main"
+import Mypage from "@/components/Mypage"
+import CreatePage from "@/components/CreatePage"
+import CreateButton from "@/components/CreateButton"
 
 export default function Home() {
   const [auth, setAuth] = React.useState(false);
--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,6 +1,8 @@
 "use client";
 
 import * as React from 'react';
+import Login from "@/components/Login";
+
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -12,56 +14,60 @@ import Menu from '@mui/material/Menu';
 
 export default function Header(props) {
     const [anchorEl, setAnchorEl] = React.useState(null);
+    const [loginOpen, setLoginOpen] = React.useState(false);
 
     const handleMenu = (event) => {
         setAnchorEl(event.currentTarget);
     };
 
-    const handleClose = () => {
+    const handleMenuClose = () => {
         setAnchorEl(null);
     };
 
     return (
-        <AppBar position="static" color='default' sx={{ marginBottom: '20px' }}>
-            <Toolbar>
-                <Typography variant="h6" component="div" sx={{ cursor: 'pointer', flexGrow: 1 }} onClick={() => { props.setContent("Main"); }}>
-                    Marry Invite
-                </Typography>
-                {props.auth ? (
-                    <div>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleMenu}
-                            color="inherit"
-                        >
-                            <AccountCircle />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorEl}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={Boolean(anchorEl)}
-                            onClose={handleClose}
-                        >
-                            <MenuItem onClick={() => { props.setContent("Mypage"); handleClose(); }}>Mypage</MenuItem>
-                            <MenuItem onClick={() => { props.setContent("Main"); props.setAuth(false); handleClose(); }}>Logout</MenuItem>
-                        </Menu>
-                    </div>
-                ) : (
-                    <Button onClick={() => { props.setAuth(true) }} color='default'>Login</Button>
-                )}
-            </Toolbar>
-        </AppBar>
+        <React.Fragment>
+            <AppBar position="static" color='default' sx={{ marginBottom: '20px' }}>
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{ cursor: 'pointer', flexGrow: 1 }} onClick={() => { props.setContent("Main"); }}>
+                        Marry Invite
+                    </Typography>
+                    {props.auth ? (
+                        <div>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleMenu}
+                                color="inherit"
+                            >
+                                <AccountCircle />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorEl)}
+                                onClose={handleMenuClose}
+                            >
+                                <MenuItem onClick={() => { props.setContent("Mypage"); handleMenuClose(); }}>Mypage</MenuItem>
+                                <MenuItem onClick={() => { props.setContent("Main"); props.setAuth(false); handleMenuClose(); }}>Logout</MenuItem>
+                            </Menu>
+                        </div>
+                    ) : (
+                        <Button onClick={() => { setLoginOpen(true); }} color='default'>Login</Button>
+                    )}
+                </Toolbar>
+            </AppBar>
+            <Login loginOpen={loginOpen} setLoginOpen={setLoginOpen} setAuth={props.setAuth}/>
+        </React.Fragment>
     );
 }
