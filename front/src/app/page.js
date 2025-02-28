48bd09a454a3fdcc30f6fd43c743c87eb3490bce
jeonginwoo <jiw413@naver.com>
2025-02-28 21:07:59 +0900

    update: prettier format

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -1,17 +1,17 @@
 "use client";
 
-import * as React from 'react';
+import * as React from "react";
 import Image from "next/image";
 import styles from "@/styles/page.module.css";
 
-import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
 
-import Header from "@/components/Header"
-import Main from "@/components/Main"
-import Mypage from "@/components/Mypage"
-import CreatePage from "@/components/CreatePage"
-import CreateButton from "@/components/CreateButton"
+import Header from "@/components/Header";
+import Main from "@/components/Main";
+import Mypage from "@/components/Mypage";
+import CreatePage from "@/components/CreatePage";
+import CreateButton from "@/components/CreateButton";
 
 export default function Home() {
   const [auth, setAuth] = React.useState(false);
@@ -19,29 +19,28 @@ export default function Home() {
 
   let contentControl = null;
   if (content == "Main") {
-    contentControl =
+    contentControl = (
       <>
         <Main />
         {auth && <CreateButton setContent={setContent} />}
-      </>;
+      </>
+    );
   } else if (content == "Mypage") {
-    contentControl =
+    contentControl = (
       <>
         <Mypage />
         {auth && <CreateButton setContent={setContent} />}
-      </>;
+      </>
+    );
   } else if (content == "CreatePage") {
-    contentControl =
-      <CreatePage />;
+    contentControl = <CreatePage />;
   }
 
   return (
     <React.Fragment>
       <CssBaseline />
       <Header setContent={setContent} setAuth={setAuth} auth={auth} />
-      <Container>
-        {contentControl}
-      </Container>
+      <Container>{contentControl}</Container>
     </React.Fragment>
   );
 }
--- front/src/components/Login.jsx
+++ front/src/components/Login.jsx
@@ -1,62 +1,80 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 
 export default function Login(props) {
-    return (
-        <Dialog
-            open={props.loginOpen}
-            onClose={() => { props.setLoginOpen(false); }}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
+  return (
+    <Dialog
+      open={props.loginOpen}
+      onClose={() => {
+        props.setLoginOpen(false);
+      }}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle sx={{ textAlign: "center" }}>Login</DialogTitle>
+      <DialogContent>
+        <Button
+          onClick={() => {
+            props.setAuth(true);
+            props.setLoginOpen(false);
+          }}
+          sx={{
+            color: "white",
+            background: "#03c75a",
+            width: "100%",
+            marginTop: "10px",
+            fontWeight: "600",
+          }}
         >
-            <DialogTitle sx={{ textAlign: "center"}}>Login</DialogTitle>
-            <DialogContent>
-                <Button
-                    onClick={() => { props.setAuth(true); props.setLoginOpen(false); }}
-                    sx={{
-                        color: "white",
-                        background: "#03c75a",
-                        width: "100%",
-                        marginTop: "10px",
-                        fontWeight: "600"
-                    }}
-                >
-                    {/* <Image src={naverLogo} width={40} height={40} alt="네이버 로고" /> */}
-                    NAVER
-                </Button>
-                <Button
-                    onClick={() => { props.setAuth(true); props.setLoginOpen(false); }}
-                    sx={{
-                        color: "rgb(0 0 0 / 85%)",
-                        background: "#FEE500",
-                        width: "100%",
-                        marginTop: "10px",
-                        fontWeight: "600"
-                    }} >
-                    {/* <Image src={kakaoLogo} width={20} height={20} alt="카카오 로고" /> */}
-                    KAKAO
-                </Button>
-                <Button
-                    onClick={() => { props.setAuth(true); props.setLoginOpen(false); }}
-                    sx={{
-                        color: "#5f6368",
-                        background: "#ffffff",
-                        width: "100%",
-                        marginTop: "10px",
-                        fontWeight: "600"
-                    }}
-                >
-                    {/* <Image src={googleLogo} width={20} height={20} alt="구글 로고" /> */}
-                    GOOGLE
-                </Button>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={() => { props.setLoginOpen(false); }}>Close</Button>
-            </DialogActions>
-        </Dialog>
-    );
+          {/* <Image src={naverLogo} width={40} height={40} alt="네이버 로고" /> */}
+          NAVER
+        </Button>
+        <Button
+          onClick={() => {
+            props.setAuth(true);
+            props.setLoginOpen(false);
+          }}
+          sx={{
+            color: "rgb(0 0 0 / 85%)",
+            background: "#FEE500",
+            width: "100%",
+            marginTop: "10px",
+            fontWeight: "600",
+          }}
+        >
+          {/* <Image src={kakaoLogo} width={20} height={20} alt="카카오 로고" /> */}
+          KAKAO
+        </Button>
+        <Button
+          onClick={() => {
+            props.setAuth(true);
+            props.setLoginOpen(false);
+          }}
+          sx={{
+            color: "#5f6368",
+            background: "#ffffff",
+            width: "100%",
+            marginTop: "10px",
+            fontWeight: "600",
+          }}
+        >
+          {/* <Image src={googleLogo} width={20} height={20} alt="구글 로고" /> */}
+          GOOGLE
+        </Button>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          onClick={() => {
+            props.setLoginOpen(false);
+          }}
+        >
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
