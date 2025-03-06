fe05a918644523fdadf4326d1a54071a556499ff
jeonginwoo <jiw413@naver.com>
2025-03-07 02:25:16 +0900

    fix: Header 오류 수정

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,5 +1,4 @@
 "use client";
-
 import * as React from "react";
 import { useRouter } from "next/navigation";
 import { useAtom } from "jotai";
@@ -12,37 +11,22 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-import AccountCircle from "@mui/icons-material/AccountCircle";
 import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
-
-// import Button from "@mui/material/Button";
 import Avatar from "@mui/material/Avatar";
 import ClickAwayListener from "@mui/material/ClickAwayListener";
 import Grow from "@mui/material/Grow";
 import Paper from "@mui/material/Paper";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import Popper from "@mui/material/Popper";
-// import MenuItem from "@mui/material/MenuItem";
 import MenuList from "@mui/material/MenuList";
-import Stack from "@mui/material/Stack";
 
 import PersonIcon from "@mui/icons-material/Person";
 import Logout from "@mui/icons-material/Logout";
 
 export default function Header() {
   const [user, setUser] = useAtom(userAtom);
-  const [anchorEl, setAnchorEl] = React.useState(null);
   const [loginOpen, setLoginOpen] = React.useState(false);
   const router = useRouter();
-
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-  };
 
   const [open, setOpen] = React.useState(false);
   const anchorRef = React.useRef(null);
@@ -80,11 +64,11 @@ export default function Header() {
   return (
     <React.Fragment>
       <AppBar position="static" color="default">
-        <Toolbar>
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
           <Typography
             variant="h6"
             component="div"
-            sx={{ cursor: "pointer", flexGrow: 1 }}
+            sx={{ cursor: "pointer" }}
             onClick={() => {
               router.push("/");
             }}
@@ -127,9 +111,9 @@ export default function Header() {
                           onKeyDown={handleListKeyDown}
                         >
                           <MenuItem
-                            onClick={() => {
+                            onClick={(event) => {
                               router.push("/mypage");
-                              handleMenuClose();
+                              handleClose(event);
                             }}
                           >
                             <ListItemIcon>
@@ -138,10 +122,10 @@ export default function Header() {
                             Mypage
                           </MenuItem>
                           <MenuItem
-                            onClick={() => {
+                            onClick={(event) => {
                               router.push("/");
                               setUser(null);
-                              handleMenuClose();
+                              handleClose(event);
                             }}
                           >
                             <ListItemIcon>
