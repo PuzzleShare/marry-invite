7ee5e88567660e8a80976f7e84add302d75d53d0
minjae <minjagot@gmail.com>
2025-03-18 17:18:55 +0900

    style remove log

--- front/src/request/hook/users.js
+++ front/src/request/hook/users.js
@@ -2,7 +2,6 @@
 
 const useUser = async () => {
   try {
-    console.log('env', process.env.NODE_ENV);
     const { data } = await axios.get(`${process.env.NEXT_PUBLIC_BACK_END}/api/login`);
     return data;
   } catch (error) {
