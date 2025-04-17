657901e8ae9d51ffea77fcb1d86d6a4e73cd065e
minjae <minjagot@gmail.com>
2025-04-17 17:49:10 +0900

    feat: 청첩장 화면 SSR로 메타데이터 설정

--- front/src/components/invite/blocks/BlockContainer.jsx
+++ front/src/components/invite/blocks/BlockContainer.jsx
@@ -9,8 +9,20 @@ import {
   TextBlock,
 } from "@/components/invite/blocks";
 
+const blockMap = {
+  blocks: (block, index) => <NestedBlock block={block} path={[index]} />,
+  text: (block, _) => <TextBlock block={block} />,
+  gallery: (block, _) => <GalleryBlock block={block} />,
+  guest_book: (block, _) => <GuestbookBlock block={block} />,
+  calendar: (block, _) => <CalendarBlock block={block} />,
+  map: (block, _) => <MapBlock block={block} />,
+};
+
 export default function BlockContainer({ block, index }) {
-  return blockType(block, index);
+  if (!blockMap[block.type] || block.style?.display === "none") {
+    return null;
+  }
+  return blockMap[block.type](block, index);
 }
 
 const blockType = (block, index) => {
--- front/src/components/invite/blocks/GuestbookBlock.jsx
+++ front/src/components/invite/blocks/GuestbookBlock.jsx
@@ -1,3 +1,4 @@
+"use client";
 import * as React from "react";
 import { useAtom } from "jotai";
 import { commentsAtom } from "@/atoms/comment";
