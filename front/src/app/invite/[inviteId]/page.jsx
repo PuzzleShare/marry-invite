a94336969522396d6d4fb10e3bd878281a2c5670
minjae <minjagot@gmail.com>
2025-04-20 14:51:12 +0900

    fix: ssr

--- front/src/app/invite/[inviteId]/page.jsx
+++ front/src/app/invite/[inviteId]/page.jsx
@@ -2,29 +2,11 @@ import { getInvite } from "@/api/invite/invite";
 import * as React from "react";
 import Content from "./_components/Content";
 
-// metadata 설정해주는 함수
-export async function generateMetadata({ params, searchParams }, parent) {
-  const { inviteId } = await params;
-  const invite = await getInvite(inviteId);
-  const previousImages = [invite?.imageUrl];
-
-  return {
-    title: invite?.data.title || "marry-invite",
-    openGraph: {
-      images: previousImages,
-    },
-  };
-}
-
 export default async function SharePage({ params }) {
   const { inviteId } = await params;
   const invite = await getInvite(inviteId);
   if (!invite) {
     return <div>청첩장을 불러오는데 실패했습니다.</div>;
   }
-  return (
-    <>
-      <Content blockData={invite.data} />
-    </>
-  );
+  return <Content blockData={invite.data} />;
 }
